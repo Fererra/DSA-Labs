156bda6d9868a9724454e349dc60babad66037ef
Fererra <nikita.sukhorukoff@gmail.com>
2025-05-26 13:20:26 +0300

    fix: fix weight matrix generation

--- Lab6/src/utils/matrix_utils.c
+++ Lab6/src/utils/matrix_utils.c
@@ -178,7 +178,7 @@ int **generateW(int **Aundir) {
   int **W = createIntMatrix();
 
   for (int i = 0; i < n; i++) {
-    for (int j = i; j < n; j++) {
+    for (int j = i + 1; j < n; j++) {
       W[i][j] = W[j][i] = (D[i][j] + H[i][j] * Tr[i][j]) * C[i][j];
     }
   }
